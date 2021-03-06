111f49bd064398c6d76fb7d3e05ddcfb76563c80
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-03-06 15:17:03 +0900

    first

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container : {
+      flex : 1,
+      justifyContent:'center',
+      alignItems:'center'
+  }
+});
+
+export default App;
