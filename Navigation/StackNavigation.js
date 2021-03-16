9141bf0f02da35a8de089c5df8dc815324becfcf
rohguentak <rhgt78@gmail.com>
2021-03-16 21:45:24 +0900

    네비게이션에 임시 home 화면이랑 authloading 추가

--- Navigation/StackNavigation.js
+++ Navigation/StackNavigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Onboarding from '../Screen/Onboarding';
-//import AuthLoading from '../Screen/AuthLoadingScreen';
+import AuthLoading from '../Screen/AuthLoadingScreen';
 import Signin from '../Screen/SigninScreen';
+import Home from '../Screen/Home';
 
 const Stack= createStackNavigator();
 
@@ -10,5 +11,7 @@ export default () => (
     <Stack.Navigator>
         <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}}/>
         <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
+        <Stack.Screen name="AuthLoading" component={AuthLoading} options={{headerShown: false}}/>
+        <Stack.Screen name="Home" component={Home}/>
     </Stack.Navigator>
 )
