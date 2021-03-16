5748d7731abb4e58f8e4032749b88eab3a0ce31d
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-03-16 15:52:30 +0900

    Add Onboarding page

--- App.js
+++ App.js
@@ -7,79 +7,30 @@
  */
 
 import React, {Component} from 'react';
+import Onboarding from './Screen/Onboarding';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  Button,
-  Image,
-  StatusBar,
+  Text
 } from 'react-native';
 
-class Cat extends Component{
-  render () {
-    let catimg=''
-
-    if(this.props.type=='one'){
-      catimg=require('./assets/cat.jpeg');
-    }
-    else if(this.props.type=='two'){
-      catimg=require('./assets/cat2.png');
-    }
-
-    return(
-      <View>
-      <Image source={catimg}style={{height:200, width:300}} />
-      </View>
-    );
-  }
-}
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state={
-      address:''
-    }
-  }
-  writeAddress=()=>{
-    this.setState({
-      address:'경기도 수원시'
-    })
-  }
-  resetAddress=()=>{
-    this.setState({
-      address:''
-    })
-  }
 
   render(){
   return (
     <View style={styles.container}>
-      <Text style={styles.textstyle}>Hello world!</Text>
-      <Cat type='one' />
-      <Cat type='two'/>
-      <Text>{this.state.address}</Text>
-      <Button title={'Write my address'} onPress={this.writeAddress}/>
-      <Button title={'Reset my address'} onPress={this.resetAddress}/>
+      <Onboarding/>
     </View>
   );
   }
 };
 
 const styles = StyleSheet.create({
-  container : {
-      flex : 1,
-      justifyContent:'center',
-      alignItems:'center'
-  },
-  textstyle:{
-    fontSize:20,
-    fontWeight:'bold'
+  container:{
+    flex:1
   }
 });
+
 
 export default App;
---
+++ Screen/Onboarding.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import colors from '../src/colors';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    Text,
+  } from 'react-native';
+
+
+class Onboarding extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.textposition}><Text style={styles.textstyle}>공부 하자</Text></View>
+                <TouchableOpacity style={styles.buttonposition} >
+                <Text style={styles.buttonstyle}>Get Started</Text>
+                </TouchableOpacity>
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'space-around',
+    },
+    textposition:{
+        position: 'absolute',
+        left: '27%',
+        top: '20%'
+    },
+    textstyle:{
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: 50,
+        color: '#2E3E5C'
+    },
+    buttonposition:{
+        position: 'absolute',
+        width: 327,
+        height: 56,
+        left: '10%',
+        top: '75%',
+        backgroundColor: colors.maincolor,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 32
+    },
+    buttonstyle:{
+        color:'white',
+        fontWeight: 'bold',
+        fontSize: 20,
+    }
+  });
+
+export default Onboarding;
