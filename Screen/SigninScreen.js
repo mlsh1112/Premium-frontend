c86dbd73f85e87c40971cdcdfee2244085bc3467
rohguentak <rhgt78@gmail.com>
2021-03-16 15:36:14 +0900

    email,password 입력받아서 state 업데이트

---
+++ Screen/SigninScreen.js
@@ -0,0 +1,138 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, {Component, useState,createRef} from 'react';
+ import {
+   SafeAreaView,
+   StyleSheet,
+   ScrollView,
+   View,
+   Text,
+   Button,
+   Image,
+   StatusBar,
+   TextInput,
+   TouchableOpacity,  
+ } from 'react-native';
+ 
+ 
+ const Signin = () => {
+   const [userEmail,setUserEmail] = useState('');
+   const [userPassword,setUserPassword] = useState('');
+   //const [loading,setLoading] = useState('');
+   //const [errortext,SetErrorText]=useState('');
+   const passwordInputRef = createRef();
+  
+   const handleSubmitPress = ()=>{
+     if (!userEmail){
+       alert("Please enter Email");
+       return ;
+     }
+     else if (!userPassword){
+       alert("Please enter Password")
+       return ;
+     }
+     console.log(userEmail);
+     console.log(userPassword);
+   }
+   return (
+     <View style={styles.container}>
+        <View>
+            <Text>Welcome</Text>
+        </View>
+        <View style={styles.FormStyle}>
+            <TextInput style={styles.InputStyle}
+              placeholder={"Email"} 
+              onChangeText={userEmail => setUserEmail(userEmail)}
+              keyboardType="email-address"
+              onSubmitEditing={() => {
+                  passwordInputRef.current.focus();
+                }
+              }
+            />
+        </View>
+        <View style={styles.FormStyle}>
+            <TextInput style={styles.InputStyle}
+              placeholder={"Password"}
+             onChangeText={userPassword => setUserPassword(userPassword)}
+             ref={passwordInputRef}
+             secureTextEntry={true}
+             returnKeyType="next"
+            />
+        </View>
+        <View >
+            <TouchableOpacity style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text style={styles.SignUpStyle}
+              //onPress={() => navigation.navigate('SignUpScreen')}
+              >
+              Sign Up
+            </Text>
+        </View>
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container : {
+       flex : 1,
+       justifyContent:'center',
+       alignItems:'center',
+   },
+   textstyle:{
+     fontSize:20,
+     fontWeight:'bold'
+   },
+   FormStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+   InputStyle: {
+    flex: 1,
+    color: "black",
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  SignUpStyle: {
+    color: "blue",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+ });
+ 
+ export default Signin;
+ 
