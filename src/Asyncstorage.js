ca138a2d68f8591bc9fd72f7520d46be20ea1a21
rohguentak <rhgt78@gmail.com>
2021-03-22 14:32:09 +0900

    deltoken 함수 삭제

--- src/Asyncstorage.js
+++ src/Asyncstorage.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
-
+//import { AsyncStorage } from 'react-native';
 
 export const setToken = async(token) => { //asyncstorage token 생성
     try{
@@ -7,15 +7,6 @@ export const setToken = async(token) => { //asyncstorage token 생성
     }
     catch (error){
         console.log("AsyncStorage setting Error: " + error.message);
-    };
-};
-
-export const delToken = async() => { //asyncstorage token 삭제
-    try{
-        await AsyncStorage.removeItem('token');
-    }
-    catch (error){
-        console.log("AsyncStorage delete Error: " + error.message);
     };
 };
 
