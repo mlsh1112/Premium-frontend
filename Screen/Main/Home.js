0deeccba6ec637592afeecd65a48d3ee56f56223
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-03-23 15:23:38 +0900

    picker UI 수정

--- Screen/Main/Home.js
+++ Screen/Main/Home.js
@@ -15,14 +15,18 @@ class Home extends Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.textposition}>따 숲</Text>
-                <RNPickerSelect 
-                    onValueChange={(value) => console.log(value)}
-                    items={[
-                        { label: '국어', value: 'korean' },
-                        { label: '영어', value: 'english' },
-                        { label: '수학', value: 'math' },
-                    ]}
-                />
+                <View style={styles.pickerstyle}>
+                    <RNPickerSelect 
+                        placeholder={{ label: "과목", value: null }}
+                        onValueChange={(value) => console.log(value)}
+                        useNativeAndroidPickerStyle={false}
+                        items={[
+                            { label: '전체', value: 'all' },
+                            { label: '국어', value: 'korean' },
+                            { label: '영어', value: 'english' },
+                            { label: '수학', value: 'math' },
+                       ]}
+                /></View>
                 <ScrollView >
                     <Text style={styles.welcome}>Welcome to React Native</Text>
                     <Text style={styles.welcome}>Welcome to React Native</Text>
@@ -41,12 +45,18 @@ const styles = StyleSheet.create({
     },
     textposition:{
         marginTop:70,
-        marginLeft:50,
+        marginLeft:30,
         fontSize:30,
         fontWeight:'bold'
     },
-    pickerposition:{
-        margin:10
+    pickerstyle:{
+        margin:30,
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 8,
+        paddingRight: 30,
     },
     welcome: {
         backgroundColor: 'orange',
