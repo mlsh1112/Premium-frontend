7e83fcfaeabf45caad7d4a4ee1f52d1553a75c0e
rohguentak <rhgt78@gmail.com>
2021-03-28 16:27:00 +0900

    project form 헤더 제거 및 오타 수정

--- Navigation/CreateProjectStackNav.js
+++ Navigation/CreateProjectStackNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import ProjectForm from '../Screen/makeProjectScreen/projectForm';
+import ProjectForm from '../Screen/makeProjectScreen/ProjectForm';
 import Profile from '../Screen/Main/Profile';
 
 
@@ -10,7 +10,7 @@ export default function createProjectStackNav(){
     return (
         <StackForCreatePrj.Navigator initialRouteName='Profile'>
             <StackForCreatePrj.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-            <StackForCreatePrj.Screen name="ProjectForm" component={ProjectForm} options={{headerShown: false}}/>
+            <StackForCreatePrj.Screen name="ProjectForm" component={ProjectForm}/>
         </StackForCreatePrj.Navigator>
     );
 }
