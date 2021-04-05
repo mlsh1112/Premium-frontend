b3af60d98855b5802176bab6cd9918a6a66b5aa0
rohguentak <rhgt78@gmail.com>
2021-04-05 12:53:17 +0900

    프로젝트 생성 폼 헤더 title 변경

--- Navigation/CreateProjectStackNav.js
+++ Navigation/CreateProjectStackNav.js
@@ -10,7 +10,7 @@ export default function createProjectStackNav(){
     return (
         <StackForCreatePrj.Navigator initialRouteName='Profile'>
             <StackForCreatePrj.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-            <StackForCreatePrj.Screen name="ProjectForm" component={ProjectForm}/>
+            <StackForCreatePrj.Screen name="ProjectForm" component={ProjectForm} options={{headerTitle: "프로젝트 생성"}}/>
         </StackForCreatePrj.Navigator>
     );
 }
