034ddabffb6861154e62f323dd1c92ed986f8b3f
rohguentak <rhgt78@gmail.com>
2021-04-10 00:14:06 +0900

    학교 인증 UI 구현

---
+++ Screen/SchoolAuth/SchoolAuth.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import colors from '../../src/colors';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    Text,
+  } from 'react-native';
+  
+import {Button} from '../../src/components/Button';
+import Imagepicker from '../../src/image-picker';
+
+const SchoolAuth = (props) => {
+    const submitPhoto = () => {
+        console.log('사진제출')
+    }
+        return (
+            <View style={styles.container}>
+                
+                <View style={styles.ImagepickerStyle}>
+                <Text style={styles.info}>공부 하자는 튜터인증을 위해 학교 인증을 진행 하고있습니다. 학교 인증을 위해 학생증과 함께 
+자신의 모습이 나오게 사진을 찍어 전송해주세요</Text>
+                    <Imagepicker />
+                </View>
+                <View styles={styles.confirm}>
+                    <Button  onPress={submitPhoto}>사진 제출</Button>
+                </View>
+          </View>
+        );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      padding: 15,
+    },
+    ImagepickerStyle: {
+        flex: 1,
+    },
+    info: {
+        margin: 10,
+        padding: 10,
+        textAlign: 'center',
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+    },
+  });
+
+export default SchoolAuth;
