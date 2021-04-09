92d86c1438e58c0b3002f4a4962b0407c763737c
rohguentak <rhgt78@gmail.com>
2021-04-10 00:14:53 +0900

    학교인증 화면과 프로젝트 인증 화면 추가

--- Navigation/CreateProjectStackNav.js
+++ Navigation/CreateProjectStackNav.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import ProjectForm from '../Screen/makeProjectScreen/ProjectForm';
 import Profile from '../Screen/Main/Profile';
-
+import AuthNamvi from './AuthNavigation';
+import SchoolAuth from '../Screen/SchoolAuth/SchoolAuth';
 
 const StackForCreatePrj = createStackNavigator();
 
@@ -11,6 +12,8 @@ export default function createProjectStackNav(){
         <StackForCreatePrj.Navigator initialRouteName='Profile'>
             <StackForCreatePrj.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
             <StackForCreatePrj.Screen name="ProjectForm" component={ProjectForm} options={{headerTitle: "프로젝트 생성"}}/>
+            <StackForCreatePrj.Screen name="Authentication" component={AuthNamvi} />
+            <StackForCreatePrj.Screen name="SchoolAuth" component={SchoolAuth} options={{headerTitle: "학교 인증"}}/>
         </StackForCreatePrj.Navigator>
     );
 }
