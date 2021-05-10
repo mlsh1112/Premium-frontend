5cbdcb07410eceffa04bc9c4bf709533fcb36a93
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-05-10 14:05:07 +0900

    iamport android setting

--- android/app/src/main/java/com/first/MainApplication.java
+++ android/app/src/main/java/com/first/MainApplication.java
@@ -30,6 +30,14 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
           return packages;
         }
+       /* @Override
+        protected List<ReactPackage> getPackages() {
+          return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+                new IamportPackage(), // 아임포트 패키지를 추가합니다.
+                new RNCWebViewPackage(), // 리액트 네이티브 웹뷰 패키지를 추가합니다.
+          );
+        }*/
 
         @Override
         protected String getJSMainModuleName() {
