2a4b2ec9e797e236b941a7a4e810c3d0f9715060
rohguentak <rhgt78@gmail.com>
2021-05-21 05:38:26 +0900

    single file select 함수 삭제

--- src/DocumentPicker.js
+++ src/DocumentPicker.js
@@ -1,31 +1,10 @@
 import DocumentPicker from 'react-native-document-picker'
 
 
-const PickSingleFile = async() => {
-    try {
-        const res = await DocumentPicker.pick({
-            type: [DocumentPicker.types.allFiles],
-        });
-        console.log(
-            res.uri,
-            res.type,
-            res.name,
-            res.size
-        );
-        return res;
-    } catch (error) {
-        if (DocumentPicker.isCancel(error)){
-            console.log("User cancel file pick")
-            return null;
-        }else {
-            throw error;
-        }
-    }
-};
 const PickMultipleFile = async() => {
     try {
         const results = await DocumentPicker.pickMultiple({
-            type: [DocumentPicker.types.allFiles],
+            type: [DocumentPicker.types.images,DocumentPicker.types.pdf],
         });
         for (const res of results) {
             console.log(
@@ -37,17 +16,12 @@ const PickMultipleFile = async() => {
         }
         return results;
     } catch (error) {
-
         if (DocumentPicker.isCancel(error)){ 
-            //alert('Canceled from multiple doc picker');
             console.log("User cancel file pick");
-            return [];
         }else {
-            alert('Unknown Error: ' + JSON.stringify(error));
-            throw error;
-            return [];
+            console.log(error)
         }
     }
 };
 
-export { PickSingleFile,PickMultipleFile };
+export { PickMultipleFile };
