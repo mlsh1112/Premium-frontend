80750b76ee5247bb6339b80405ab62c5fa21aae6
rohguentak <rhgt78@gmail.com>
2021-05-22 00:38:50 +0900

    헤더수정

--- Navigation/AuthNavigation.js
+++ Navigation/AuthNavigation.js
@@ -20,10 +20,10 @@ export default function createProjectStackNav(){
         <StackForAuth.Navigator initialRouteName='Authentication'>
             <StackForAuth.Screen name="Authentication" component={Authentication} options={{headerShown: false}}/>
             <StackForAuth.Screen name="TutorAuthentication" component={TutorAuthentication} options={{headerTitle: "프로젝트 인증"}}/>
-            <StackForAuth.Screen name="TutorAuthCheck" component={TutorAuthCheck} options={{headerTitle: 'Tutee 인증 내용'}}/>
+            <StackForAuth.Screen name="TutorAuthCheck" component={TutorAuthCheck} options={{headerTitle: '튜티 인증 내용'}}/>
             <StackForAuth.Screen name="TuteeAuthentication" component={TuteeAuthentication} options={{headerTitle: "프로젝트 인증"}}/>
             <StackForAuth.Screen name="AuthPayBack" component={AuthPayBack} options={{headerShown: false}} />
-            <StackForAuth.Screen name="ProjectDetail" component={ProjectDetail} />
+            <StackForAuth.Screen name="ProjectDetail" component={ProjectDetail} options={{headerTitle: '프로젝트 상세보기'}}/>
             <StackForAuth.Screen name="TuteeAuthList" component={TuteeAuthList}/>
             <StackForAuth.Screen name="TutorAuthList" component={TutorAuthList}/>
             <StackForAuth.Screen name="TuteeAuthdetail" component={TuteeAuthdetail}/>
