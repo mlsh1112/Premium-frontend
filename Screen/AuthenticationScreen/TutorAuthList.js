d3b7960cf19e908aa610d0d09e00ea942bb44d83
rohguentak <rhgt78@gmail.com>
2021-05-22 00:58:06 +0900

    날짜계산 moment사용 및 refactoring

--- Screen/AuthenticationScreen/TutorAuthList.js
+++ Screen/AuthenticationScreen/TutorAuthList.js
@@ -1,83 +1,79 @@
-import React, { Component, useState, useEffect } from 'react';
-import { View,Text,TouchableOpacity,ImageBackground,ScrollView,Image } from 'react-native';
-import { getproject,gettutorprojs } from '../../src/Api';
+import React, { useState, useEffect } from 'react';
+import { View,Text,TouchableOpacity,ScrollView,Image } from 'react-native';
+import { gettutorprojs } from '../../src/Api';
 import colors from '../../src/colors'
-import {  Card} from 'react-native-paper';
-import AsyncStorage from '@react-native-community/async-storage';
+import { Card } from 'react-native-paper';
 import authBtn from '../../assets/authBtn-001.png'
+
+const moment = require("moment");
+
 const ProjectAuthCard = ({navigation,project}) => {
-    console.log(project)
-    var startDate=new Date().getTime() - new Date(project.started_at).getTime();
-    var remainDay=Math.floor(startDate / (1000 * 60 * 60 * 24))
-    var pastDay=project.experience_period-remainDay
+    const startDate = moment(project.started_at)
+    const now = moment()
+    const remainDay = now.diff(startDate,'days') //시작하는 날짜가 0일차
+    const pastDay = project.duration - remainDay
+
     return( 
             <View style={{flexDirection:'row'}}>
-            <Card >
-                <View style={cardstyles.card} >
-                    <View style={{margin:15, marginLeft:27}}>
-                    <Text style={cardstyles.textstyle}>{project.title}</Text>
-                    <View style={{marginTop:5}}>
-                        {project.experience_period>=remainDay+1?
-                        <View style={{flexDirection:'row',margin:2}}>
-                        <Text style={cardstyles.dayStyle}>{remainDay+1} </Text>
-                        <Text style={cardstyles.dayStyle2}>일차 인증</Text>
+                <Card>
+                    <TouchableOpacity onPress={()=> navigation.navigate('ProjectDetail',{project})}>
+                        <View style={cardstyles.card}>
+                            <View style={{marginVertical: 10,marginHorizontal: 15}}>
+                                <Text style={cardstyles.textstyle}>{project.title}</Text>
+                                <View>
+                                    {   remainDay >= 0
+                                        ?   <View style={{flexDirection:'row',margin:2}}>
+                                                <Text style={cardstyles.dayStyle}>{remainDay + 1} </Text>
+                                                <Text style={cardstyles.dayStyle2}>번째 인증</Text>
+                                            </View>
+                                        :   <View style={{flexDirection:'row',margin:2}}>
+                                                <Text style={cardstyles.dayStyle}>프로젝트</Text>
+                                                <Text style={cardstyles.dayStyle2}> 시작 전입니다.</Text>
+                                            </View>
+                                    }
+                                    { pastDay < 1
+                                        ? <Text style={cardstyles.dayStyle3}>프로젝트가 끝났습니다.</Text>
+                                        : <Text style={cardstyles.dayStyle3}>남은 인증 {pastDay}개</Text>
+                                    }
+                                </View>
+                            </View>
                         </View>
-                        :
-                        <View style={{flexDirection:'row',margin:2}}>
-                        <Text style={cardstyles.dayStyle}>{project.experience_period} </Text>
-                        <Text style={cardstyles.dayStyle2}>일차 인증</Text>
-                        </View>
-                        }
-                        {
-                        pastDay-1<0?
-                        <Text style={cardstyles.dayStyle3}>프로젝트가 끝났습니다.</Text>
-                        :
-                        <Text style={cardstyles.dayStyle3}>남은 인증 {pastDay-1}일</Text>
-
-                    }
-                    </View>
-                    </View>
-                </View>
-            </Card>
-
-            <TouchableOpacity style={{}} onPress={()=>{navigation('TutorAuthentication',{project})}}>
-                    <Image source={authBtn} style={{width:90,height:100}}></Image>
                     </TouchableOpacity>
+                </Card>
+                <TouchableOpacity onPress={()=>{navigation.navigate('TutorAuthentication',{project})}}>
+                    <Image source={authBtn} style={{width:90,height:100}}></Image>
+                </TouchableOpacity>
             </View>
-
     )
 }
 
 const TutorAuthList = ({navigation}) => {
     const [projects, setProjects] = useState()
-    
     useEffect(()=>{
-
-        const callApi= async()=>{
-            await gettutorprojs()
-            .then(res=>setProjects(res.data))
-            .catch(err=>console.log(err))
-        }
-        callApi()
+        console.log('get current user')
+        gettutorprojs().then(res => {
+            setProjects(res.data)
+        }).catch(e => {
+            console.log('---------get_project_list error--------------')
+            console.log(e)
+        })
     },[])
+
     return (
         <View style={styles.container}>
-            {
-                projects?
-                <ScrollView>
-                    {projects.map((project,index)=>{
+            { projects
+                ?   <ScrollView>
+                        {projects.map((project,index)=>{
                             return  <View style={{margin:10, backgroundColor:colors.maincolor,borderRadius:20,marginBottom:20}} key={index}>
-                            <ProjectAuthCard navigation={navigation.navigate} project={project}  ></ProjectAuthCard></View>
+                            <ProjectAuthCard navigation={navigation} project={project}  ></ProjectAuthCard></View>
                         })
                     }
-                </ScrollView>
-                :
-                <Text>진행 중인 프로젝트가 없습니다</Text>
+                    </ScrollView>
+                :   <Text>진행 중인 프로젝트가 없습니다</Text>
             }
         </View>
     )
 }
-
 
 const styles={
     container:{
@@ -118,7 +114,7 @@ const cardstyles={
     textstyle:{
         fontWeight:'bold',
         fontSize:20,
-        margin:2
+        marginHorizontal:2
     },
     dayStyle:{
         fontWeight:'bold',
@@ -132,9 +128,7 @@ const cardstyles={
     dayStyle3:{
         fontSize:15,
         color:"#9E9E9E",
-        margin:2
+        marginHorizontal:2
     }
-
-    
 }
 export default TutorAuthList;
