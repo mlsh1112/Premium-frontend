3c4d8c90857a2eec53e4561cd258c63b4727fe59
rohguentak <rhgt78@gmail.com>
2021-05-27 19:31:58 +0900

    1일짜리 일정 렌더링하도록 수정

--- Screen/makeProjectScreen/PreviewCalendar.js
+++ Screen/makeProjectScreen/PreviewCalendar.js
@@ -13,9 +13,9 @@ import colors from '../../src/colors'
 const moment = require("moment");
 
 function PreviewCalendar(props){
-  console.log('=================schedule=================')
-  console.log(props.schedule)
-  console.log('==========================================')
+  // console.log('=================schedule=================')
+  // console.log(props.schedule)
+  // console.log('==========================================')
   
   const [markedDates,setmarkedDates]=useState()
   const [agenda,setAgenda] = useState()
@@ -33,19 +33,36 @@ function PreviewCalendar(props){
             endDate = endDate.subtract(1,'days')
         }
         const subnum = endDate.diff(startDate,'days')
-        for(let j=1;j<=subnum;j++){
+        if(subnum === 0){
           Object.assign(tempagenda,
             {[startDate.format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]},
-            {[moment(startDate).add(j,"d").format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]},
             {[endDate.format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]}
           )
           Object.assign(tempschedule,
             {[startDate.format("YYYY-MM-DD")]:{startingDay: true, color: color[colorIndex][0] , textColor: 'white'}},
-            {[moment(startDate).add(j,"d").format("YYYY-MM-DD")]:{color: color[colorIndex][1], textColor: 'white'}},
             {[endDate.format("YYYY-MM-DD")]:{endingDay: true, color:color[colorIndex][0] , textColor: 'white'}}
           )
+        }else {
+          for(let j=1;j<=subnum;j++){
+            Object.assign(tempagenda,
+              {[startDate.format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]},
+              {[moment(startDate).add(j,"d").format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]},
+              {[endDate.format("YYYY-MM-DD")]:[{title: props.chapters[index].title}]}
+            )
+            Object.assign(tempschedule,
+              {[startDate.format("YYYY-MM-DD")]:{startingDay: true, color: color[colorIndex][0] , textColor: 'white'}},
+              {[moment(startDate).add(j,"d").format("YYYY-MM-DD")]:{color: color[colorIndex][1], textColor: 'white'}},
+              {[endDate.format("YYYY-MM-DD")]:{endingDay: true, color:color[colorIndex][0] , textColor: 'white'}}
+            )
+          }
         }
       })
+      console.log('-------------------- agenda ----------------------')
+      console.log(tempagenda)
+      console.log('--------------------------------------------------')
+      console.log('=================schedule=================')
+      console.log(tempschedule)
+      console.log('==========================================')
       setmarkedDates(tempschedule);
       setAgenda(tempagenda)
   },[])
