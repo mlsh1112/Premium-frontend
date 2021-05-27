b5084e2bd4a5348be4cc4b1813b2552c78d7d20b
rohguentak <rhgt78@gmail.com>
2021-05-27 17:47:12 +0900

    채팅방 ui 수정

--- Screen/chatting/Message.js
+++ Screen/chatting/Message.js
@@ -1,7 +1,9 @@
 import React,{useState,useEffect} from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
-import {Alert} from 'react-native'
+import { GiftedChat,Bubble,Send } from 'react-native-gifted-chat';
+import {Alert,View,ActivityIndicator} from 'react-native'
 import firebase,{firestore} from '../../FirebaseConfig/Firebase'
+import colors from '../../src/colors'
+import { IconButton } from 'react-native-paper';
 
 const Messsage = (props) => {
     console.log('////////////////////////////////')
@@ -82,13 +84,44 @@ const Messsage = (props) => {
             createdAt,
             text,
             user
-        }).then((docRef)=> {
+        }).then(()=> {
         console.log('message from ',myinfo.id)
         }).catch((e)=> {
             console.log(e)
         })
     }
-
+    function renderBubble(props){
+        return (
+            <Bubble
+              {...props}
+              wrapperStyle={{right: {backgroundColor: colors.maincolor},left: {backgroundColor: colors.subcolor}}}
+              textStyle={{right: {color: '#fff'},left : {color : 'rgba(0,0,0,0.7)'}}}
+            />
+          );
+    }
+    function renderSend(props){
+        return(
+            <Send {...props}>
+                <View style={{justifyContent:'center',alignItems:'center'}}>
+                  <IconButton icon='send-circle' size={32} color={colors.subcolor2} />
+                </View>
+            </Send>
+        )
+    }
+    function scrollToBottomComponent(){
+        return (
+            <View style={{justifyContent:'center',alignItems:'center'}}>
+              <IconButton icon='chevron-double-down' size={36} color='rgba(0,0,0,0.3)' />
+            </View>
+          );
+    }
+    function renderLoading(){
+        return (
+            <View style={{justifyContent:'center',alignItems:'center'}}>
+              <ActivityIndicator size='large' color='#6646ee' />
+            </View>
+          );
+    }
     return(
         <GiftedChat 
             messages={messages}
@@ -97,6 +130,14 @@ const Messsage = (props) => {
                 _id: myinfo.id,
                 name: myinfo.name
                 }}
+            placeholder={'메세지를 입력하세요'}
+            isLoadingEarlier={true}
+            renderBubble={renderBubble}
+            alwaysShowSend={true}
+            renderSend={renderSend}
+            scrollToBottom={true}
+            scrollToBottomComponent={scrollToBottomComponent}
+            renderLoading={renderLoading}
         />
     )
 }
