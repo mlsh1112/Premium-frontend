a72992aa0975b090fbc1023e0ed5f290d4b9c6c3
rohguentak <rhgt78@gmail.com>
2021-06-01 01:52:07 +0900

    fix picker to RNpickerselect

--- src/utils/RenderChapter.js
+++ src/utils/RenderChapter.js
@@ -4,23 +4,43 @@ import {
     View,
     Text,
     TouchableOpacity,
+    Dimensions,
 } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
-import {makelist} from './MakePickerItem'
+import {makelist,makePickerItemlist} from './MakePickerItem'
 import colors from '../colors';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import RNPickerSelect from 'react-native-picker-select';
+
 
 export function RenderChapter({chapters,deletechapter}){
     console.log(chapters)
       return(
                   <View style={styles.chapterlist}>
                     {chapters.map((chapter,key) => {
+                        console.log(chapter.weight)
                       return(
                         <View key={key} style={{flex:1,height:60,flexDirection:'row',alignItems: 'center',justifyContent: 'space-between',marginVertical:3}}>
                             <Text style={styles.textStyle}>
                               {chapter.title}
                             </Text>
-                            <View style={{width:' 25%',backgroundColor:'white',borderWidth:1,borderRadius: 20,borderColor: colors.subcolor}}>
+                                {/* <View style={styles.pickerstyle}> */}
+                                  <RNPickerSelect
+                                      value={chapter.weight}
+                                      useNativeAndroidPickerStyle={false}
+                                      onValueChange={(value) => {
+                                        chapter.weight = value
+                                        console.log(chapter)
+                                      }}
+                                      items={makePickerItemlist(1,10)}  
+                                      style={{ ...pickerSelectStyles }}
+                                      placeholder={{}}
+                                      fixAndroidTouchableBug={true}
+                                    />
+                                  
+                                {/* </View>   */}
+                            {/* <View style={{width:' 25%',backgroundColor:'white',borderWidth:1,borderRadius: 20,borderColor: colors.subcolor}}>
                               <Picker
                                 selectedValue={chapters[key].weight}
                                 style={{ height: '100%', width: '100%',}}
@@ -32,7 +52,7 @@ export function RenderChapter({chapters,deletechapter}){
                               >
                                 {makelist(10)}
                               </Picker>
-                            </View>
+                            </View> */}
 
                             <TouchableOpacity
                               onPress={(e)=>{
@@ -66,8 +86,9 @@ export function RenderChapter({chapters,deletechapter}){
             marginBottom: 10,
             justifyContent: 'space-between',
         },
+        
         pickerstyle:{
-          width: "20%",
+          width: "25%",
           marginTop:'1%',
           paddingVertical: 10,
           paddingHorizontal: 12,
@@ -78,4 +99,34 @@ export function RenderChapter({chapters,deletechapter}){
           backgroundColor: 'white',
         },
       });
+      const pickerSelectStyles = StyleSheet.create({
+        inputIOS: {
+            fontSize: 16,
+            height:'100%',
+            width:Dimensions.get('screen').width * 0.20,
+            backgroundColor:'white',
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderWidth: 1,
+            borderColor: colors.maincolor,
+            borderRadius: 15,
+            color: 'black',
+            paddingRight: 30, // to ensure the text is never behind the icon
+        },
+        inputAndroid: {
+          fontSize: 16,
+          height:'100%',
+          width:Dimensions.get('screen').width * 0.20,
+          textAlign:'center',
+          backgroundColor:'white',
+          paddingHorizontal: 10,
+          paddingVertical: 8,
+          borderWidth: 1,
+          borderColor: colors.maincolor,
+          borderRadius: 15,
+          color: 'black',
+          
+        },
     
+        
+      });
