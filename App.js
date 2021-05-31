fa28a563fb5005e00a6e95baaf8ed8ab6aaf8ef9
rohguentak <rhgt78@gmail.com>
2021-06-01 06:27:48 +0900

    current user context 설정

--- App.js
+++ App.js
@@ -6,55 +6,24 @@
  * @flow strict-local
  */
 
- import axios from 'axios';
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import Stack from "./Navigation/StackNavigation";
-import test from "./config"
-
-
-const BookAPI = axios.create({
-  headers: { 'Authorization': test },
-});
 import {decode, encode} from 'base-64'
+import {CurrentUser} from './src/utils/CurrentUser'
+
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
 
-import {
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
-
 export default function App(){
+  const [context, setContext] = useState();
   return (
-    <NavigationContainer>
-      <Stack />
-    </NavigationContainer>
+    <CurrentUser.Provider value={[context, setContext]}>
+      <NavigationContainer>
+        <Stack />
+      </NavigationContainer>
+    </CurrentUser.Provider>
   )
 }
-//class App extends Component {
-//  
-//  render(){
-//  return (
-//    <NavigationContainer>
-//      <Stack />
-//    </NavigationContainer>
-//    //<View style={styles.container}>
-//    //  <Onboarding/>
-//    //  {/* <Signin />*/} 
-//    //</View>
-//  );
-//  }
-//};
 
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1
-  }
-});
-
-
-//export default App;
