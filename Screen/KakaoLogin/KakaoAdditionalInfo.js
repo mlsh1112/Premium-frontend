bce14ad3faa20d785a982a90a7c672bb847a6905
rohguentak <rhgt78@gmail.com>
2021-06-02 04:24:35 +0900

    카카오 추가정보 기입 ui 변경

--- Screen/KakaoLogin/KakaoAdditionalInfo.js
+++ Screen/KakaoLogin/KakaoAdditionalInfo.js
@@ -1,10 +1,13 @@
 import React, { useState,useEffect} from 'react';
 import {Button} from '../../src/components'
+import colors from '../../src/colors'
 import {
    StyleSheet,
    View,
    Text,
    TextInput,
+   Alert,
+   TouchableOpacity
  } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import {userUpdate} from '../../src/Api';
@@ -38,22 +41,23 @@ const KakaoAdditionalInfo=(props)=>{
         //setToken(res.data.token);
       }).then(() => {
         console.log('update success')
-        props.navigation.replace('Main');
+        props.navigation.replace('AuthLoading');
       }).catch(error => {
-        alert("이메일 혹은 패스워드를 확인해주세요.")
+        Alert.alert("유저정보 업데이트에 실패하였습니다.", "중복된 전화번호는 가입이 불가합니다.")
         console.log(error);
       });
   }  
 
   return(
     <View style={styles.container}>
-     <View >
-       <Text style={styles.title}>회원 가입을 위한 추가정보를 입력해주세요</Text>
+     <View style={styles.topstyle}>
+       <Text style={styles.title}>Create Account</Text>
+       <Text style={styles.nametitle}>{props.route.params.userinfo.name} 님  안녕하세요.</Text>
      </View>
      <Formik
        style={styles.FormStyle}
        validationSchema={validationSchema}
-       initialValues={{ phoneNumber: '' }}
+       initialValues={{ userName:'',phoneNumber: '' }}
        onSubmit={values => {
          //console.log(values)
          handleSubmitPress(values)
@@ -62,31 +66,34 @@ const KakaoAdditionalInfo=(props)=>{
        {({ handleChange, handleBlur, handleSubmit, values, errors,touched,}) => (
        
          <>
-           <Text style={styles.subtitle}>1. 전화 번호</Text>
+           <Text style={styles.subtitle}>전화번호 (- 없이 입력)</Text>
            <TextInput
              name="phone-number"
-             placeholder="010-XXXX-XXXX"
+             placeholder="     전화번호 (- 없이 입력)     "
              style={styles.textInput}
              onChangeText={handleChange('phoneNumber')}
              onBlur={handleBlur('phoneNumber')}
              value={values.phoneNumber}
-             keyboardType="number-pad"
            />
            {(errors.phoneNumber && touched.phoneNumber) &&
            <Text style={styles.errorText}>{errors.phoneNumber}</Text>
            }
            
-           <View>
-              <Text style={styles.subtitle}>당신은 튜터 입니까?</Text>
+           <View style={{marginVertical:30,flexDirection:'row'}}>
+              <Text style={styles.typeQtxt}>Tutor로 가입하시겠습니까?</Text>
               <CheckBox
                 value={isSelected}
                 onValueChange={setSelection}
                 style={styles.checkbox}
+                lineWidth={3}
+                tintColor={'white'}
               />
-           </View>
-           <View style={styles.button}>
-             <Button onPress={handleSubmit}>추가 정보 기입 완료하기</Button>
-           </View>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={styles.buttonposition} onPress={handleSubmit}>
+                <Text style={styles.buttonstyle}>가입하기</Text>
+              </TouchableOpacity>
+          </View>
          </>
        )}
      </Formik>
@@ -97,44 +104,137 @@ const KakaoAdditionalInfo=(props)=>{
 
 const styles = StyleSheet.create({
   container : {
-      flex : 1,
-      justifyContent:'center',
-      alignItems:'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom:10,
-  },
-  FormStyle: {
-   flexDirection: 'row',
-   height: 40,
-   marginTop: 20,
-   marginLeft: 35,
-   marginRight: 35,
-   margin: 10,
- },
-  checkbox:{
-    alignSelf:"center",
-  },
-  errorText: {
-    fontSize: 12,
-    color: 'red',
-  },
-  textInput: {
-    height: 40,
-    width: '90%',
-    margin: 10,
-    backgroundColor: 'white',
-    borderWidth: 1,
-    borderRadius: 10,
-    fontSize:15,
-    fontWeight:'bold'
-  },
-  subtitle: {
-    width: "90%",
-    fontWeight:"bold",
-    fontSize: 18,
+    flex : 1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:colors.maincolor,
+},
+topstyle:{
+  marginVertical:30
+},
+title: {
+  color:'white',
+  fontSize:33,
+  fontWeight:'bold',
+  elevation: 5,
+  shadowColor: '#5B7459',
+  shadowOffset: { width: 0, height: 5 },
+  shadowOpacity: 0.5,
+  shadowRadius: 5,
+},
+textstyle:{
+  fontSize:20,
+  fontWeight:'bold'
+},
+FormStyle: {
+ flexDirection: 'row',
+ height: 40,
+ marginTop: 20,
+ marginLeft: 35,
+ marginRight: 35,
+ margin: 10,
+},
+InputStyle: {
+ flex: 1,
+ color: "black",
+ paddingLeft: 15,
+ paddingRight: 15,
+ borderWidth: 1,
+ borderRadius: 30,
+ borderColor: '#dadae8',
+},
+SignUpQStyle: {
+ color: "black",
+ textAlign: 'center',
+ fontSize: 14,
+ alignSelf: 'center',
+ padding: 10,
+},
+SignUpStyle: {
+ color: "blue",
+ textAlign: 'center',
+ fontWeight: 'bold',
+ fontSize: 14,
+ alignSelf: 'center',
+ padding: 5,
+},
+checkbox:{
+alignSelf:"center",
+marginLeft:40,
+},
+lable:{
+margin:9,
+},
+errorText: {
+  fontSize: 12,
+  color: 'red',
+},
+textInput: {
+  height: 50,
+  width: '90%',
+  marginVertical: 10,
+  backgroundColor: 'white',
+  borderRadius: 10,
+  fontSize:15,
+  fontWeight:'bold',
+  elevation: 5,
+  shadowColor: 'gray',
+  shadowOffset: { width: 0, height: 3 },
+  shadowOpacity: 0.5,
+  shadowRadius: 5,
+  borderRadius:20 
+},
+subtitle: {
+  width: "90%",
+  fontSize: 15,
+  marginLeft:40
+},
+nametitle: {
+color:'white',
+width: "90%",
+fontWeight:'bold',
+fontSize: 18,
+marginLeft:40,
+marginTop:10
+},
+typeQtxt:{
+color:'white',
+fontSize:20,
+fontWeight:'bold',
+elevation: 5,
+shadowColor: '#5B7459',
+shadowOffset: { width: 0, height: 5 },
+shadowOpacity: 0.5,
+shadowRadius: 5,
+},
+PickerBox: {
+position:'absolute',
+height: 40,
+width: '35%',
+margin: 10,
+backgroundColor: 'white',
+borderWidth: 1,
+borderRadius: 10,  
+},
+buttonposition:{
+width: 327,
+height: 50,
+backgroundColor: 'white',
+justifyContent: "center",
+alignItems: "center",
+borderRadius: 32,
+elevation: 5,
+shadowColor: 'grey',
+shadowOffset: { width: 0, height: 5 },
+shadowOpacity: 0.5,
+shadowRadius: 5,
+borderRadius:20 
+  
+},
+buttonstyle:{
+color:colors.maincolor,
+fontWeight: 'bold',
+fontSize: 20,
 }
 });
 
