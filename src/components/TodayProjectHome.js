25c9de3fe5a94c0408ec465f05bbec96238adbed
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-06-02 23:35:22 +0900

    today card 수정

--- src/components/TodayProjectHome.js
+++ src/components/TodayProjectHome.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { View, Image,Text, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-paper';
 import authBtn from '../../assets/authBtn-001.png'
-
 const moment = require("moment");
 class Projectcard extends Component {
-    
     render() {
         //console.log(this.props.data)
+        const now = moment().format('YYYY-MM-DD')
         const project=this.props.data
-        const startDate = moment(project.started_at)
-        const now = moment()
-        const remainDay = now.diff(startDate,'days') //시작하는 날짜가 0일차
+        const startDate = moment(project.started_at).format('YYYY-MM-DD')
+        const realnow = moment(now)
+        const realstartDate = moment(startDate)
+        const remainDay = realnow.diff(realstartDate,'days') //시작하는 날짜가 0일차
         var pastDay = project.duration
         if(remainDay >= 0){
             pastDay = project.duration - remainDay
@@ -20,9 +20,9 @@ class Projectcard extends Component {
             <View style={{flexDirection:'row',backgroundColor:'white',marginLeft:25,borderRadius:8,height:110}}>
             <Card >
                 <View style={styles.card} >
-                    <View style={{margin:15, marginLeft:27}}>
+                    <View style={{justifyContent:'center',height:'100%',paddingHorizontal:8,}}>
                     <Text style={styles.textstyle}>{project.title}</Text>
-                    <View style={{marginTop:5}}>
+                    <View style={{}}>
                         {remainDay >= 0?
                         <View style={{flexDirection:'row',margin:2}}>
                         <Text style={styles.dayStyle}>{remainDay+1} </Text>
@@ -39,29 +39,25 @@ class Projectcard extends Component {
                         <Text style={styles.dayStyle3}>프로젝트가 끝났습니다.</Text>
                         :
                         <Text style={styles.dayStyle3}>남은 인증 {pastDay-1}개</Text>
-
                     }
                     </View>
                     </View>
                 </View>
             </Card>
-
             <TouchableOpacity onPress={()=>{ this.props.navigation.navigate('Authentication',{project})}}>
                     <Image source={authBtn} style={{width:90,height:100}}></Image>
                     </TouchableOpacity>
             </View>
-      
         );
     }
 }
-
 const styles={
     card:{
         borderWidth:0,
         borderRadius:20,
         width:270,
-        height:100,
-        flexDirection:'row'
+        height:"100%",
+        justifycontent:'center'
     },
     textstyle:{
         fontSize:19,
@@ -81,9 +77,5 @@ const styles={
         color:"#9E9E9E",
         margin:2
     }
-
-    
 }
- 
-
 export default Projectcard;
