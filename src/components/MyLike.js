8ef62bde9e81f9b168616bb9adc5ad768c0fd3fd
rohguentak <rhgt78@gmail.com>
2021-06-03 01:37:37 +0900

    사용자 프로필사진 렌더링 수정

--- src/components/MyLike.js
+++ src/components/MyLike.js
@@ -1,19 +1,13 @@
 import React, { useState,useEffect } from 'react';
 import {
-    StyleSheet,
     View,
     Text,
     Image,
-    TouchableOpacity,
-    Card,
-    ScrollView
   } from 'react-native';
 import colors from '../colors';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {getmylikes,islike,getlikes} from '../Api';
+import {getlikes} from '../Api';
 import cat from '../../assets/cat2.png' 
 import Like from './Like'
-import axios from 'axios'
 
 const RenderLikeList = (props) => {
     return (
@@ -22,7 +16,10 @@ const RenderLikeList = (props) => {
                 return(
                     <View style={{margin: 10,height:80,flexDirection:'row',flexWrap:'wrap',borderTopLeftRadius:20}} key={index}>
                         <View style={{backgroundColor:colors.maincolor,height:'100%',width:'20%',justifyContent:'center',alignItems:'center',borderTopLeftRadius:20}}>
-                            <Image source={cat} style={{width:60,height:60,borderRadius:30}}/>
+                            {tutor.likable.image === ' '
+                               ? <Image source={cat} style={{width:60,height:60,borderRadius:30}}/>
+                               : <Image source={{uri: tutor.likable.image}} style={{width:60,height:60,borderRadius:30}}/>
+                            }
                         </View>
                         <View style={{flexDirection:'row',backgroundColor:'white',width:'80%',height:'100%',padding:5,justifyContent:'space-between',alignItems:'center'}}>
                             <Text style={{fontSize:20,fontWeight:'bold'}}>{tutor.likable?.name}</Text>
